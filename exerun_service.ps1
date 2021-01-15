374c23b42538159e6d53ed19d23773b1122d55f7
sabaszx <5910110573@psu.ac.th>
2021-01-15 16:00:39 +0700

    add force flag

--- exerun_service.ps1
+++ exerun_service.ps1
@@ -1,6 +1,4 @@
 ### restart service through powershell script
 ## these script have issues, I will fix it soon :)
-Restart-Service 'BioStar 2 MariaDB(x64)' 
-Restart-Service 'BioStar 2 MariaDB(x64)'
-Restart-Service 'BioStar 2 Launcher(x64)'
-Restart-Service 'BioStar 2 Web Server(x64)'
+Restart-Service 'BioStar 2 MariaDB(x64)'  -Force #force service
+Restart-Service 'BioStar 2 Launcher(x64)' -Force #force service

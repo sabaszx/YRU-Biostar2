9b53dbad8f8fc7dafcec20275ee96a0accec303e
sabaszx <5910110573@psu.ac.th>
2021-01-18 11:11:06 +0700

    zip lock

--- exerun_service.ps1
+++ exerun_service.ps1
@@ -1,4 +1,5 @@
 ### restart service through powershell script
 ## these script have issues, I will fix it soon :)
 Restart-Service 'BioStar 2 MariaDB(x64)'  -Force #force service
-Restart-Service 'BioStar 2 Launcher(x64)' -Force #force service
+#Restart-Service 'BioStar 2 Launcher(x64)' -Force #force service
+Start-Service 'BioStar 2 Launcher(x64)' -Confirm
--- sql_table_build.py
+++ sql_table_build.py
@@ -11,10 +11,11 @@ print(date_format)
 #current table format is t_lgYYYYMM
 current_format = 't_lg' + now.strftime('%Y') + now.strftime('%m')
 
+## connection credentials settings
 connection = pymysql.connect(
-    host='', #host 
-    user='', #username
-    password='', #password
+    host='10.10.2.97', #host 
+    user='root', #username
+    password='P@ssw0rd', #password
     port=3312,
     db='biostar2_ac'
 )
@@ -23,7 +24,7 @@ connection = pymysql.connect(
 curr = connection.cursor()
 
 #in order to build log table daily, create table.
-create_schema_string = 'CREATE TABLE t_lg' + date_format + 'SELECT * FROM t_lg202101' 
+create_schema_string = 'CREATE TABLE t_lg' + date_format + 'SELECT * FROM ' + current_format 
 
 #execute command
 curr.execute(create_schema_string)

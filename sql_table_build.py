635ed01577ee724207074b2d0264627cac47eca1
sabaszx <5910110573@psu.ac.th>
2021-01-19 09:18:08 +0700

    clear cell and re-setting filename

--- sql_table_build.py
+++ sql_table_build.py
@@ -32,6 +32,6 @@ curr.execute(create_schema_string)
 #export current schema into csv file
 sql_query = pd.read_sql_query('select * from biostar2_ac.t_lg'+ date_format + 'as', connection)
 df = pd.DataFrame(sql_query)
-df.to_csv(r'date_format'+'.csv', index = False)
+df.to_csv(r'exported_{}.csv'.format(date_format), index = False)
 
 connection.close()

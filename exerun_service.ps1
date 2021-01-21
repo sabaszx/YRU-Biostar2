18d0d82887249cc3b3a93e9aedada21a4cc11118
sabaszx <5910110573@psu.ac.th>
2021-01-21 08:55:03 +0700

    set timeout off

--- exerun_service.ps1
+++ exerun_service.ps1
@@ -5,5 +5,3 @@ Restart-Service 'BioStar 2 MariaDB(x64)'  -Force #force service
 Start-Service 'BioStar 2 Launcher(x64)' -Confirm:$false
 ### execute python file
 python .\sql_table_build.py
-
-timeout.exe /t 10

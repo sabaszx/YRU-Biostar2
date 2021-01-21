f7f34217cf0e57aae0a7d805326876964834de4b
sabaszx <5910110573@psu.ac.th>
2021-01-21 13:14:55 +0700

    hide credentials

--- sql_table_build.py
+++ sql_table_build.py
@@ -13,9 +13,9 @@ current_format = 't_lg' + now.strftime('%Y') + now.strftime('%m')
 
 ## connection credentials settings
 connection = pymysql.connect(
-    host='10.10.2.97', #host 
-    user='root', #username
-    password='P@ssw0rd', #password
+    host='', #host 
+    user='', #username
+    password='', #password
     port=3312,
     db='biostar2_ac'
 )
